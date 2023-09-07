d49a4af4029923c1778e9688e6925dff5603061d
Mahfoud-Sa <binsabbah2013@gmail.com>
2023-09-07 09:56:09 +0300

    add feature

---
+++ SimpleApi/app/Http/Controllers/articles.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class articles extends Controller
+{
+    //
+    function getData(){
+        return ['hellow'=>'Anil'];
+    }
+}
